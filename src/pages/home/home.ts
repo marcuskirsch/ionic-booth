feffed8c16e59536337b2620f21d4ac07155da51
Bengt Weiße <bengtler@gmail.com>
2017-06-08 19:36:27 +0200

    Add gallery, ability to create images, store images persistent in external app dir, read app dir at initial startup to fill slideshow

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,63 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+import { Camera } from '@ionic-native/camera';
+import { File, Entry } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  images: string[] = [];
+  isCordova = true;
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera,
+    private file: File,
+    private platform: Platform
+  ) {
+    this.isCordova = platform.is('cordova');
+  }
+
+  ngOnInit() {
+    if (!this.isCordova) {
+      return;
+    }
+    const pathParts = this.file.externalDataDirectory.split('/');
+    pathParts.pop();
+    const dir = pathParts.pop();
+    this.file.externalDataDirectory.split('/').pop()
+    this.file.listDir(pathParts.join('/'), dir).then((result) => {
+      result.forEach((entry) => {
+        if (entry.isFile) {
+          this.addImage(entry);
+        }
+      })
+    })
+  }
+
+  addPhoto() {
+    this.camera.getPicture({
+      allowEdit: true,
+      targetWidth: 1920,
+      targetHeight: 1080,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      quality: 80
+    }).then((fileUri: string) => {
+      const pathParts = fileUri.split('/');
+      const fileName = pathParts.pop();
+      const path = pathParts.join('/');
+
+      return this.file.moveFile(path, fileName, this.file.externalDataDirectory, `${Date.now()}_marriage_${fileName}`)
+    }).then((entry: Entry) => this.addImage(entry));
+  }
+
+  private addImage(entry: Entry) {
+    this.images.push(this.getFilePath(entry));
+  }
+
+  private getFilePath(entry: Entry): string {
+    return `${this.file.externalDataDirectory}${entry.name}`;
   }
 }
